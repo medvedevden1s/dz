b448a52587ea46d75210cfdbba4949b41a173320
medvedevden1s <uaadmin@DMEDVEDEV.levi9.com>
2019-02-06 17:11:23 +0200

    added svg circle icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import VerticalLinearStepper from './Stepper'
+import Stepper from './Stepper'
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
-  render() {
-    return (
-      <body>
+  function getSteps() {
+    return ['Set name and durationn', 'Sipping address', 'Select account', 'Confirm your reserv'];
+  }
+
+ const App = () => {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  function handleNext() {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  }
+
+  // function handleReset() {
+  //   setActiveStep(0);
+  // }
+
+  return (
+      <div>
         <div id="main">
-          <article>Article</article>
+          <article>
+            <Button disabled={activeStep === 0} onClick={handleBack}>Back</Button>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+          </article>
           <aside>
-            <VerticalLinearStepper />
+            <Stepper steps={steps} activeStep={activeStep}></Stepper>
+            
+            <svg height="100" width="100">
+              <circle cx="35" cy="35" r="12" stroke="orange" strokeWidth="2" fill="white" />
+              <text x="35%" y="35%" text-anchor="middle" stroke="orange" stroke-width="1px" dy=".3em">1</text>
+            </svg>
+
           </aside>
         </div>
         <footer className="footer">
-            <div className="logo"><a href="#">RESERVE</a></div>
-            <div className="exit"><a href="#">Exit X</a></div>
+            <div className="logo"><a href="http://localhost:3000/#">RESERVE</a></div>
+            <div className="exit"><a href="http://localhost:3000/#">Exit X</a></div>
         </footer>
-      </body>     
+      </div>     
     );
-  }
 }
 
 export default App;
